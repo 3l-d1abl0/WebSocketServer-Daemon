ed40c6e943add9f04063dc076a1fd4fabe9a3868
Sameer Barha <sameer.barha12@gmail.com>
2016-07-05 11:47:24 +0530

    stable spawn

--- spawn.py
+++ spawn.py
@@ -5,42 +5,77 @@ import time
 import os
 import signal
 
-FILE_PATH = "/home/ubuntu/background/Demo/server.php"
-FILE = "server.php"
+_FILE_PATH = "/home/ubuntu/background/Demo/server.php"
+_FILE = "server.php"
+#The Screen Name for the Websocket Server
+_SCREEN_NAME ="websocketserver"
+_WS = False
+_WEBSOCKET_URL = "ws://localhost:8080/serverPush"
 
-def screen_present(name):
+def server_check(name):
         var = subprocess.check_output(["screen -ls; true"],shell=True)
-        if "."+name+"\t(" in var:
-                print name+" is running"
+        if "."+name+"\t(" in var.decode('ascii'):
+            #return True
+            
+            print('%s : Attempting to Connect to Server .... \n' % ( time.ctime(time.time()) ))
+            try:
+                _WS = create_connection(_WEBSOCKET_URL)
+                print('%s : Successfully Connected to Server !\n'%(time.ctime(time.time())))
+                
+                if(type(_WS).__name__ is 'WebSocket'):
+                    print('%s : CLosing WebSocket Server Connection ... \n'%(time.ctime(time.time())))
+                    _WS.close()
+                
+                #Server is Accepting Requests
+                return True
+            
+            except Exception as ex:
+                #LOG could NOT connect to server
+                #LOG ex.args and TIME
+                print('%s : Could not Connect to Server ... \n'%(time.ctime(time.time())))
+                template = "An exception of type {0} occured.\nArguments:\n{1!r}"
+                message = template.format(type(ex).__name__, ex.args)
+                print (message)
+                #Server Cannot Be Connected
+                return False
+        
         else:
-                print name+" is not running"
+            #No Screen Started for Server Available
+            return False
+
 
 def spawn():
-    '''
-    command = 'screen -dmS websocketserver php %s'%(FILE_PATH)
-    print ('Command :: ',command)
-    p = subprocess.Popen(['screen', '-d', '-m', '-S', 'websocketserver', 'php', FILE_PATH], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    #Form the proper Command
+    command = "screen -dmS websocketserver php {}".format(_FILE)
     
-    print (err)
-    print (output)
-    '''
-    
-    command = "screen -dmS websocketserver php {}".format(FILE)
     print ('Command: ', command)
+    
     process = subprocess.Popen(command, shell=True,  stdout=subprocess.PIPE)
     #Gives bytes, which is a binary sequence of bytes rather than a string of Unicode characters
-    op = process.communicate()[0]
+    #op = process.communicate()[0]
+    
     #wait for Exitcoede
     exit_code = process.wait()
-    print ('Exit COde : ',exit_code)
+    if exit_code <0:
+        print ('Error while Executing Command : EXIT_STATUS %s'%(exit_code))
+    else:
+        print ('Server Started ...')
     #Close the Pipe
     process.stdout.close()
+    #decoded_string = op.decode('ascii')
+    #print('Decoded String : ', decoded_string)
     
-    decoded_string = op.decode('ascii')
-    print('Decoded String : ', decoded_string)
-
+    
+    
 if __name__ == "__main__":
     print ('--MAIN--')
     spawn()
-    screen_present("websocketserver")
+    time.sleep(10)
+   # server_check("websocketserver")
+    
+    if server_check(_SCREEN_NAME) is True:
+        print ("There is a Websocketserver Screen !")
+    else:
+        print ("There is no websocketserver Screen Running !")
+        
+        
