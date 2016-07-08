5e767c37803d5e8210d8b8b2d2a985fcf4b86d70
Sameer Barha <sameer.barha12@gmail.com>
2016-07-08 18:37:33 +0530

    added cwd to Spawn subprocess

--- webSocketDaemon.py
+++ webSocketDaemon.py
@@ -1,14 +1,40 @@
 #python ./webSocketDaemon.py start
+#python ./webSocketDaemon.py stop
 
-import logging
-import time
 #third party libs
+from websocket import create_connection
 from daemon import runner
+#system
+import subprocess
+import logging
+import signal
+import time
+import json
+import sys
+import os
 
+
+#Defaults for the Logger
 _LOGGER = False
 _HANDLER = False
-_LOG_FILE = '/home/ubuntu/background/python/testdaemon.log'
-
+_LOG_FILE = '/home/ubuntu/background/Demo/webSocketDaemon.log'
+#Defaults for the Daemon
+_STDOUT_PATH = '/home/ubuntu/background/Demo/daemon/daemon.log'
+_STDERR_PATH = '/home/ubuntu/background/Demo/daemon/daemon.log'
+_PIDFILE_PATH ='/home/ubuntu/background/Demo/daemon/daemon.pid'
+_PIDFILE_TIMEOUT = 3
+#WebSocket Server Url to Connect
+_WEBSOCKET_URL = "ws://localhost:8080/serverPush"
+#Screen Session Name For Server
+_SCREEN_NAME ="websocketserver"
+#Server File Name
+_SERVER_FILE ="server.php"
+#Current Working Directory (location of server Name)
+_CWD='/'
+#WebSocket Connection Object
+_WS = False
+#Time interval to check server (in seconds)
+_INTERVAL = 10
 
 def setup_logger():
     global _LOGGER
@@ -18,43 +44,197 @@ def setup_logger():
     #Set the FileHandler for Logger
     handler = logging.FileHandler(_LOG_FILE)
     #Set The Logging Format
-    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%d-%m-%Y %H:%M:%S'))
+    #handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%d-%m-%Y %H:%M:%S'))
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', '%d-%m-%Y %H:%M:%S'))
     _LOGGER.addHandler(handler)
     
     return handler
 
-class App():
+def is_daemon_running():
+    global _PIDFILE_PATH
+    global _PIDFILE_TIMEOUT
+    lockfile = runner.make_pidlockfile(_PIDFILE_PATH, _PIDFILE_TIMEOUT)
+    if lockfile.is_locked():
+        return True
+    return False
+
+class Monitor():
 
     def __init__(self):
         self.stdin_path = '/dev/null'
-        self.stdout_path = '/home/ubuntu/background/python/stdout'
-        self.stderr_path = '/home/ubuntu/background/python/stderr'
-        self.pidfile_path = '/home/ubuntu/background/python/testdaemon.pid'
-        self.pidfile_timeout = 5
+        self.stdout_path = _STDOUT_PATH
+        self.stderr_path = _STDERR_PATH
+        self.pidfile_path = _PIDFILE_PATH
+        self.pidfile_timeout = _PIDFILE_TIMEOUT
+
+    ##Makes a Connection to WebSocketServer to check if gets Response
+    def ping_websocketserver(self):
+        _LOGGER.info('Pinging the Server %s ...'%(_WEBSOCKET_URL))
+        try:
+            _WS = create_connection(_WEBSOCKET_URL)
+            _LOGGER.info('Connected to Server %s ...'%(_WEBSOCKET_URL))
+            if(type(_WS).__name__ is 'WebSocket'):
+                _LOGGER.info('Closing connection to %s ...'%(_WEBSOCKET_URL))
+                #LOG Connection Closed to Server
+                _WS.close()
+            #Connected to Server
+            return True
+
+        except Exception as ex:
+            _LOGGER.warn('Could not connect to %s ...'%(_WEBSOCKET_URL))
+            template = "An exception of type {0} occured.\nArguments:\n{1!r}"
+            _LOGGER.error(template.format(type(ex).__name__, ex.args))
+            return False
+    
+    ##checks if there is a process running for the WebSocketServer
+    def check_websocketserver(self):
+        _LOGGER.info('Checking for websocket Server Process ...')
+        command = "ps aux | grep \"{}\" | grep -v  \"/bin/sh\" | grep -v \"grep\" | head -1 | awk '{{ print $2 }}' ".format(_SERVER_FILE)
+        _LOGGER.info('Command: %s'%(command))
+        process = subprocess.Popen(command, shell=True,  stdout=subprocess.PIPE)
+        #Gives bytes, which is a binary sequence of bytes rather than a string of Unicode characters
+        op = process.communicate()[0]
+        #Close the Pipe
+        process.stdout.close()
+        #Decode to ASCII ## EXCEPTION if its HEX or any other Format
+        pid = op.decode('ascii').strip()
+        _LOGGER.info('Decoded PID : %s'%(pid))
+
+        #Check if we have a PID for the WebSocket Server 
+        if pid and pid.isdigit():
+            return pid
+        else:
+            # There is no Server Running
+            _LOGGER.warn('No PID for websocket Server')
+            return False
+
+    ##Tries to Start a WebSocketServer via Screen Session
+    def spawn(self):
+        #Form the proper Command
+        command = "screen -dmS websocketserver php {}".format(_SERVER_FILE)
+        _LOGGER.info('Command : %s'%(command))
+
+        process = subprocess.Popen(command, shell=True, cwd=_CWD, stdout=subprocess.PIPE)
+        #Gives bytes, which is a binary sequence of bytes rather than a string of Unicode characters
+        #op = process.communicate()[0]
+        #wait for Exitcoede
+        exit_code = process.wait()
+        #Close the Pipe
+        process.stdout.close()
+        #decoded_string = op.decode('ascii')
+        #print('Decoded String : ', decoded_string)
+
+        if exit_code <0:
+            _LOGGER.warn('Error while executing command : EXIT_STATUS : %s'%(exit_code))
+            return False
+        else:
+            _LOGGER.info('command ran successfully ... ')
+            return True
+    
+    ##Checks if there is Screen Session running for WebSocketServet and tries to Connect it
+    def new_server_check(self):
+        var = subprocess.check_output(["screen -ls; true"],shell=True)
+        #check if _SCREEN_NAME is in output
+        if "."+_SCREEN_NAME+"\t(" in var.decode('ascii'):
+            _LOGGER.info('Screen Session is Running ... ')
+            return self.ping_websocketserver()
+        
+        else:
+            #No Screen Started for Server
+            _LOGGER.warn('Screen session couldn\'t be started ... ')
+            return False
 
     def run(self):
         while True:
-            #Main code goes here ...
-            #Note that logger level needs to be set to logging.DEBUG before this shows up in the logs
-            _LOGGER.debug("Debug message")
-            _LOGGER.info("Info message")
-            
             #captures TraceBack
             #logger.error('Failed to open file', exc_info=True)
+            _LOGGER.info('--MAIN--')
+            #Ping the websocket Server
+            ping_result = self.ping_websocketserver()
             
-            _LOGGER.warn("Warning message")
-            _LOGGER.error("Error message")
-            time.sleep(10)
+            ##Testing Setting ping_result to False
+            ping_result = False
+            
+            #If Cannot Connect to WebSocketServer
+            if ping_result is False :
+                #Flag to check if a new Process should be started
+                to_spawn = True
+                #Check if Process (Server) is Running
+                pid = self.check_websocketserver()
+                
+                #IF PID not False then its Valid
+                if pid: 
+                    _LOGGER.info('Killing process with PID : %s'%(pid))
+                    os.kill(int(pid), signal.SIGTERM)   #os.kill(int(pid), 9)
+                    _LOGGER.info('Checking if PID : %s is still Running ...'%(pid))
+                    #Waiting for /proc/pid file to disappear on process termination
+                    time.sleep(5)
+                    #Check if the Server was Successfully Killed
+                    process_path = "/proc/{}".format(pid)
+                    if os.path.exists(process_path):
+                        to_spawn = False
+                        _LOGGER.error('Could not kill Process with PID : %s'%(pid))
+                        _LOGGER.critical('Failed to Kill Existing Websocket Server !')
+                    else:
+                        to_spawn = True
+                        _LOGGER.info('Process with PID : %s successfully killed ...'%(pid))
+                        
+                #Check if new Server Should Spawn
+                if not to_spawn:
+                    _LOGGER.critical('Cannot Start a new Server Instance ! ')
+
+                else:
+                    #Spawn a New Server
+                    _LOGGER.info('Starting a new Server Instance ... ')
+                    #print('Breaking....')
+                    #sys.exit(0)
+                    spawn_result = self.spawn()    
+
+                    if spawn_result:
+                        _LOGGER.info('Wait for Server to initalize, before checking it ...')
+                        time.sleep(15)
+                        #Check is Server is actually running
+                        _LOGGER.info('Checking if Server is Running ...')
+                        if self.new_server_check():
+                            #Server is Up and Running
+                            _LOGGER.info('Server is up & running ...')
+                        else:
+                            _LOGGER.critical('Failed to start a new server Instance ...')
+                    else:
+                        _LOGGER.critical('COMMAND FAILED ! Could not start new Server Instance ... ')
+                    
+            #ping_result is True   
+            else:
+                _LOGGER.info('WebSocketServer is up & running & Listening to Connections ! ')        
+
+            _LOGGER.info('--MAIN ENDS--')
+            time.sleep(_INTERVAL)
 
 
 
 if __name__ == "__main__":
     
+    if(sys.argv[1]=='start'):
+        if is_daemon_running():
+            print('This Daemon is Already Running ! In case of Doubt check Daemon\'s PID File')
+            exit(0)
+            
     #Initialize Logger settings
     _HANDLER = setup_logger()
+    #Instantialte Daemon Class
+    serverMonitor = Monitor()
+    #Assigning current working directory path to _CWD GLobal variable 
+    _CWD =os.path.abspath(os.path.dirname('__file__'))
     
-    app = App()
-    daemon_runner = runner.DaemonRunner(app)
-    #This ensures that the logger file handle does not get closed during daemonization
-    daemon_runner.daemon_context.files_preserve=[_HANDLER.stream]
-    daemon_runner.do_action()
+    try:
+        #Start_daemon
+        daemon_runner = runner.DaemonRunner(serverMonitor)
+        #This ensures that the logger file handle does not get closed during daemonization
+        daemon_runner.daemon_context.files_preserve=[_HANDLER.stream]
+        daemon_runner.do_action()
+    except runner.DaemonRunnerStartFailureError as e:
+        print ('Could not START daemon: {0}'.format(str(e)))
+    except runner.DaemonRunnerStopFailureError as e:
+        print ('Could not STOP daemon: {0}'.format(str(e)))
+
+    
